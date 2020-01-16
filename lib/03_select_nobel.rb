751cfd8b807102bbf0a0dfb004383009b9ca9845
Combatd <combatdstar@gmail.com>
2020-01-16 11:38:33 -0800

    SELECT yr, subject where nobels.winner is Albert Einstein

--- lib/03_select_nobel.rb
+++ lib/03_select_nobel.rb
@@ -52,6 +52,14 @@ end
 def einstein_prize
   # Show the year and subject that won 'Albert Einstein' his prize.
   execute(<<-SQL)
+    SELECT
+      yr,
+      subject
+    FROM
+      nobels
+    WHERE
+      winner = 'Albert Einstein'
+
   SQL
 end
 
