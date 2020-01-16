37ee82bea2e0f3df77390edcacf273318da863fe
Combatd <combatdstar@gmail.com>
2020-01-16 14:33:12 -0800

    Select all countries from Americas where population is less than 25 million

--- lib/04_select_within_select.rb
+++ lib/04_select_within_select.rb
@@ -139,5 +139,15 @@ def sparse_continents
   # population.
   # Hint: Sometimes rewording the problem can help you see the solution.
   execute(<<-SQL)
+    SELECT
+      name,
+      continent,
+      population
+    FROM 
+      countries
+    WHERE
+      population < 25000000
+      AND
+      continent = 'Americas'
   SQL
 end
