38f576db52aad4bdb5f87a567eb270e63d578e26
Combatd <combatdstar@gmail.com>
2020-01-18 17:50:10 -0800

    SELECT id title yr from movies where the title is like Star Trek ordered by yr"

--- lib/06_joins_warmup.rb
+++ lib/06_joins_warmup.rb
@@ -53,7 +53,7 @@ def year_of_kane
       yr
     FROM
       movies
-    where
+    WHERE
       title = 'Citizen Kane'
   SQL
 end
@@ -63,6 +63,16 @@ def trek_films
   # these movies include the words Star Trek in the title). Order results by
   # year.
   execute(<<-SQL)
+    SELECT
+      id,
+      title,
+      yr
+    FROM
+      movies
+    WHERE
+      title LIKE '%Star Trek%'
+    ORDER BY
+      yr
   SQL
 end
 
