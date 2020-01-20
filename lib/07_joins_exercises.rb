d9f295be21b7e762b256de37600b946d7376fa1e
Combatd <combatdstar@gmail.com>
2020-01-20 03:55:57 -0800

    SELECT title name frmo movies joining castings and actors by movie id actors id where the movie id has the name Julie Andrews and the name is the lead

--- lib/07_joins_exercises.rb
+++ lib/07_joins_exercises.rb
@@ -134,6 +134,38 @@ def andrews_films_and_leads
   # List the film title and the leading actor for all of the films 'Julie
   # Andrews' played in.
   execute(<<-SQL)
+    SELECT
+      title,
+      name
+    FROM
+      movies
+    JOIN
+      castings
+      ON
+      movies.id = castings.movie_id
+    JOIN
+      actors
+      ON
+      castings.actor_id = actors.id
+    WHERE
+      movies.id IN (
+        SELECT 
+          movies.id
+        FROM
+          movies
+        JOIN 
+          castings 
+          ON 
+          movies.id = castings.movie_id
+        JOIN
+          actors
+          ON
+          castings.actor_id = actors.id
+        WHERE 
+          name = 'Julie Andrews'
+      )
+      AND
+      ord = 1
   SQL
 end
 
