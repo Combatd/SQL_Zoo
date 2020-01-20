600a13216a749416e8615ed6f15ad093db1f5b13
Combatd <combatdstar@gmail.com>
2020-01-19 17:56:00 -0800

    SELECT title, name from movies joining castings and actors by movie id and actor id where movies.yr 1962 and casting order is 1

--- lib/07_joins_exercises.rb
+++ lib/07_joins_exercises.rb
@@ -84,6 +84,23 @@ end
 def films_and_stars_from_sixty_two
   # List the title and leading star of every 1962 film.
   execute(<<-SQL)
+    SELECT
+      title,
+      name
+    FROM
+      movies
+    JOIN
+      castings
+      ON
+      movies.id = castings.movie_id
+    JOIN
+      actors
+      ON
+      castings.actor_id = actors.id
+    WHERE
+      movies.yr = 1962
+      AND
+      castings.ord = 1
   SQL
 end
 
