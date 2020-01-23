27cfb54b07ac8fc8f961410bbabe54d61c5f708e
Combatd <combatdstar@gmail.com>
2020-01-23 09:52:09 -0800

    SELECT all teachers.name, depts.name from teachers joining to depts by id and dept_id

--- lib/08_using_null.rb
+++ lib/08_using_null.rb
@@ -31,6 +31,13 @@ def all_teachers_join
   # Use a type of JOIN that will list all teachers and their department,
   # even if the department in NULL/nil.
   execute(<<-SQL)
+    SELECT
+      teachers.name,
+      depts.name
+    FROM
+      teachers
+    LEFT JOIN
+      depts ON depts.id = teachers.dept_id
   SQL
 end
 
